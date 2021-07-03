9c6372b02e99ec3730ca24113914a904b8b13841
jojunhui125 <86397805+jojunhui125@users.noreply.github.com>
2021-07-03 16:40:57 +0900

    BaekJoonOnlineJudge 2839 Algorithms - Math Problem

---
+++ baekjoon2839.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 
+
+
+int main() {
+	int input;
+	int count = 0;
+
+	scanf("%d", &input);
+
+	while (input > 0)
+	{
+		if (input % 5 == 0)
+		{
+			input -= 5;
+			count++;
+		}
+		else if (input % 3 == 0)
+		{
+			input -= 3;
+			count++;
+		}
+		else if (input > 5)
+		{
+			input -= 5;
+			count++;
+		}
+		else
+		{
+			count=-1;
+			break;
+		}
+	}
+	printf("%d", count);
+}
