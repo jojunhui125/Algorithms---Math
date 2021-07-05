711f2eda28961946b4464c1fea25291208282a6d
jojunhui125 <86397805+jojunhui125@users.noreply.github.com>
2021-07-05 17:14:51 +0900

    BaekJoonOnlineJudge 1978 Algorithms - Math Problem

---
+++ baekjoon1978.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS 
+
+
+int main()
+{
+    int n;
+    int num, count = 0;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &num);
+        for (int j = 2; j <= num; j++)
+        {
+            if (num == j)
+            {
+                count++;
+            }
+            if (num % j == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    printf("%d", count);
+
+	
+	return 0;
+}
+
